4a24377a39a7fb358fe555639a4c5db20b99f89a
KimHyunJin <khj745700@naver.com>
2023-11-02 20:40:11 +0900

    Websocket 다중 연결 채팅 전송

--- src/main/java/com/example/chatserver/handler/ChatHandler.java
+++ src/main/java/com/example/chatserver/handler/ChatHandler.java
@@ -7,18 +7,28 @@ import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import java.util.logging.Level;
 
 @Component
 @Slf4j
 public class ChatHandler implements WebSocketHandler {
-
+    private final Sinks.Many<String> sink = Sinks.many().multicast().directBestEffort();
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        Flux<WebSocketMessage> output = session.receive()
-                .doOnNext(data -> log.info("{}", data))
-                .map(value -> session.textMessage("Echo " + value));
+        Flux<String> output = session.receive()
+                .map(message -> message.getPayloadAsText());
 
-        return session.send(output);
-
+        output.doOnNext(s -> log.info(s));
+        output.subscribe(s -> {
+            log.info("emitData : {}, sessionId : {}", s, session.getId());
+            sink.emitNext(s, Sinks.EmitFailureHandler.FAIL_FAST); // 데이터 발행
+        });
+        Flux<WebSocketMessage> massages = sink.asFlux().log("Sinks ??? ", Level.SEVERE).map(session1 -> {
+            log.info("session1 : {}", session1);
+            return session.textMessage(session1);
+        });
+        return session.send(massages);
     }
 }
